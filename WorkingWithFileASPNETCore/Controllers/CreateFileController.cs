3212c870aa017d22956c0291724013c1a9daffee
tegarswasono <tegar.s@weefer.co.id>
2021-11-30 14:00:59 +0700

    first commit

---
+++ WorkingWithFileASPNETCore/Controllers/CreateFileController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithFileASPNETCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateFileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public CreateFileController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                StreamWriter objstreamwriter = new StreamWriter(stream);
+                objstreamwriter.Write("This is the content");
+                objstreamwriter.Flush();
+                objstreamwriter.Close();
+                return File(stream.ToArray(), "text/plain", "file.txt");
+            }
+        }
+
+        [HttpGet("SaveOnServer")]
+        public ActionResult SaveOnServer()
+        {
+            var path = Path.Combine(_env.ContentRootPath, @"Files\" + "MyTest.txt");
+            using (FileStream fs = System.IO.File.Create(path))
+            {
+                byte[] content = new UTF8Encoding(true).GetBytes("Hello World");
+                fs.Write(content, 0, content.Length);
+            }
+            return Ok("Sukses Bosku Coba Lihat di Folder Server");
+        }
+    }
+}
---
+++ WorkingWithFileASPNETCore/Controllers/UploadController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithFileASPNETCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("Single")]
+        [RequestSizeLimit(8000000)]
+        public async Task<ActionResult> Single(IFormFile file)
+        {
+            string[] permittedExtensions = { ".csv" };
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                return BadRequest("Only Allowed CSV File");
+
+            //var filePath = Path.Combine(@"c:\Project\testing2", file.FileName);
+            var filePath = Path.Combine("Upload", file.FileName);
+            using (var stream = System.IO.File.Create(filePath))
+                await file.CopyToAsync(stream);
+
+            return Ok();
+        }
+        [HttpPost("WithRename")]
+        [RequestSizeLimit(8000000)]
+        public async Task<ActionResult> WithRename(IFormFile file)
+        {
+            string[] permittedExtensions = { ".xls" };
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                return BadRequest("Only Allowed XLS File");
+
+            string fileName = file.FileName;
+            bool check = System.IO.File.Exists("Upload/" + fileName);
+            while (check)
+            {
+                fileName = fileName.Replace(".xls", "") + "_" + Helper.RandomString(5) + ".xls";
+                check = System.IO.File.Exists("Upload/" + fileName);
+            }
+
+            var filePath = Path.Combine("Upload", fileName);
+            using (var stream = System.IO.File.Create(filePath))
+                await file.CopyToAsync(stream);
+
+            return Ok();
+        }
+    }
+    public static class Helper
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray());
+        }
+    }
+}
